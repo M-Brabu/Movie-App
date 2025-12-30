a115fde58d92267f7409fcba6a7eea8080b59ed4
M-Brabu <brabumurugan18@gmail.com>
2025-12-30 14:19:42 +0530

    linked in linkadded

--- src/Home.jsx
+++ src/Home.jsx
@@ -17,6 +17,7 @@ function Home(){
     <li>About The App</li>
     <li>Get Detailed info that what are the details can get about movie</li>
   </ul>
+  <div className='h3 linkedin'>developed by : <a href="https://www.linkedin.com/in/brabu-murugan-a7a15b297?utm_source=share_via&utm_content=profile&utm_medium=member_android" className='btn btn-info ' id='linkedinbtn'>Brabu M</a></div>
 </div>
 
     </>)
