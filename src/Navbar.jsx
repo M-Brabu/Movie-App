b151afd296b29c618426eb96fe6cec2e39ea89b5
M-Brabu <brabumurugan18@gmail.com>
2025-12-30 14:00:25 +0530

    hunterz

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom"
 import camera from "./assets/camera-reels-fill.svg"
 function Navbar(){
     return(<>
-    <div className="h1 " id="navheading"><div><img className="mx-3 logo" src={camera}/>Movie App<span> </span> </div>
+    <div className="h1 " id="navheading"><div><img className="mx-3 logo" src={camera}/>Movie Hunterz<span> </span> </div>
     <div className="m-3 ">
       <Link to="/" className=" m-1 btn btn-primary" >HOME</Link>
       <Link to="/Search" className="m-1 btn btn-success">Search</Link>
